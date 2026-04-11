5164081a85e59ff6701272096e991f19e1251343
Frederick Ollinger <follinge@gmail.com>
2026-04-10 17:20:45 -0700

    Add squeectl STUB

---
+++ sandbox/TODO.md
@@ -0,0 +1,15 @@
+1. Save to file
+
+1a. Need to save the header to a file
+
+1b. Save the commands to make all the tables
+
+2. Parser
+
+3. Unit testing
+
+3a. Unit test functions to create a table and header
+
+4. Unified binary
+
+4a. Once we get our parser, we can unify all the commands in a single binary
---
+++ sandbox/squeectl.c
@@ -0,0 +1,21 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "squee-structures.h"
+
+int main(int argc, char* argv[]) {
+    printf("squeectl args [%i] \n", argc);
+    if (1 >= argc) {
+        printf("usage: \n");
+        printf("\n");
+        printf("./squeectl");
+        printf("\n");
+        return(0);
+    }
+
+    printf("squeectl [%s] \n", argv[1]);
+
+    return(0);
+}
