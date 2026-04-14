9c14f7d728e0f9377f3182d497357cae5427babe
Frederick Ollinger <follinge@gmail.com>
2026-04-13 19:25:47 -0700

    Squee Read Database STUB

--- sandbox/squee-read-database.c
+++ sandbox/squee-read-database.c
@@ -16,6 +16,11 @@ int main(int argc, char* argv[]) {
         return(0);
     }
 
+    int buffer_size = 10000;
+    char buffer[buffer_size];
+
+    printf("opening file %s \n", argv[argc - 1]);
+
     FILE *fd = fopen(argv[argc - 1], "r");
     if (NULL == fd) {
         printf("%s: Error opening to %s \n", argv[0], argv[argc - 1]);
@@ -24,38 +29,13 @@ int main(int argc, char* argv[]) {
 
     Table *tbl = squee_new_empty_table();
 
+    while(fgets(buffer, buffer_size, fd)) {
+        printf("%s", buffer);
+    }
+
     // TODO READ Table Name
 
     fclose(fd);
-
-/*
-    Table *tbl = squee_new_table(argv[1], 0, argc - 1, argv);
-    printf("tbl->name [%s] \n", tbl->name);
-    squee_print_header(tbl->header);
-
-    FILE *fd = fopen(argv[argc - 1], "w");
-    if (NULL == fd) {
-        printf("%s: Error writing to %s \n", argv[0], argv[argc - 1]);
-        return(1);
-    }
-
-    fprintf(fd, "SQUEE format 3%c", SQUEE_START_OF_TEXT);
-
-    Header *hdr_p = tbl->header;
-    while (NULL != hdr_p) {
-        if (hdr_p->field_t == SQUEE_INT) {
-            printf("main(): Field Name: %s %i ", hdr_p->field_name, hdr_p->field_t);
-            fprintf(fd, "%s%c%i%c", hdr_p->field_name, SQUEE_UNIT_SEPARATOR, hdr_p->field_t, SQUEE_RECORD_SEPARATOR);
-            printf("\n");
-        }
-        hdr_p = hdr_p->next;
-    }
-
-    fprintf(fd, "%c", SQUEE_END_OF_TEXT);
-    fprintf(fd, "%c", SQUEE_END_OF_FILE);
-
-    fclose(fd);
-*/
 
     return(0);
 }
