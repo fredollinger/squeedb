30ce3652c413c6f2e6c654d6ac177938fbcee45d
Frederick Ollinger <follinge@gmail.com>
2026-04-24 18:40:18 -0700

    Squee Read Database From File Works

--- sandbox/squee-read-database.c
+++ sandbox/squee-read-database.c
@@ -17,52 +17,9 @@ int main(int argc, char* argv[]) {
         return(0);
     }
 
-    int buffer_size = 10000;
-    int type = 0;
-    char *endptr = NULL;
-    char buffer[buffer_size];
-    char *pbuffer = buffer;
-    char *tok, *col, *type_s;
-    char squee_start_of_text[2] = { SQUEE_START_OF_TEXT, '\0' };
-    char squee_unit_separator[2] = { SQUEE_UNIT_SEPARATOR, '\0' };
-    char squee_record_separator[2] = { SQUEE_RECORD_SEPARATOR, '\0' };
-    char squee_end_of_text[2] =  { SQUEE_END_OF_TEXT, '\0' };
-    size_t len;
+    printf("Reading database: [%s] \n", argv[1]);
 
-    FILE *fd = fopen(argv[argc - 1], "r");
-    if (NULL == fd) {
-        printf("%s: Error opening to %s \n", argv[0], argv[argc - 1]);
-        return(1);
-    }
-
-    Database *db = squee_new_empty_database();
-    Header *curr_header = db->table->header;
-
-    printf("\n");
-    while(fgets(buffer, buffer_size, fd)) {
-        tok = strsep(&pbuffer, squee_start_of_text);
-        tok = strsep(&pbuffer, squee_unit_separator); 
-        len = strlen(tok);
-        db->table->name = (char*)malloc(len + 1);
-        strncpy(db->table->name, tok, len);
-
-        while(1) {
-            type_s = strsep(&pbuffer, squee_record_separator);
-            if (type_s[0] == SQUEE_END_OF_TEXT) {
-                break;
-            }
-            else if (type_s[0] == '\0') {
-                printf("Error reading file, premature end of file \n");
-                return(1);
-            }
-            col = strsep(&type_s, squee_unit_separator);
-            type = strtol(type_s, &endptr, 10);
-            curr_header = squee_header_add_column(curr_header, col, type);
-        }
-    }
-
-    fclose(fd);
-
+    Database *db = squee_read_database_from_file(argv[1]);
     squee_print_header(db->table->header);
 
     return(0);
