deb92c63314a73313441c03cdb3906ee22fa26e3
Frederick.Ollinger <follinge@gmail.com>
2026-07-03 11:51:38 -0700

    Test Add Row STUB

--- sandbox/smoke/test-squee-database.c
+++ sandbox/smoke/test-squee-database.c
@@ -8,7 +8,10 @@
 #include "squee-structures.h"
 
 int main() {
-	char *cols[] = {"John", "Doe", "42", "4.25"};
+	char *cols_header[] = {"2", "First_Name", "2", "Last_Name", "2", "Age", "0", "Hourly_Rate", "1"};
+	char *cols_data[] = {"John", "Doe", "42", "4.25"};
     Database *db = squee_new_empty_database();
-    db->table = squee_new_table_with_header("Employees", 0, 3, cols);
+    db->table = squee_new_table_with_header("Employees", 0, 3, cols_header);
+    Row *row = squee_create_row(db->table, cols_data, 8);
+    
 }
