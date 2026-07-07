a56e4ceb7ce40455c4dc75a4c57a6fc0b7c2cb33
Frederick Ollinger <follinge@gmail.com>
2026-07-06 19:21:17 -0700

    Create Row STUB

--- sandbox/smoke/test-squee-database.c
+++ sandbox/smoke/test-squee-database.c
@@ -12,5 +12,7 @@ int main() {
 	char *cols_data[] = {"John", "Doe", "42", "4.25"};
     Database *db = squee_new_empty_database();
     db->table = squee_new_table_with_header("Employees", 0, 8, cols_header);
-    // Row *row = squee_create_row(db->table, cols_data, 8);
+    squee_print_header(db->table->header);
+    Row *row = squee_create_row(db->table, cols_data, 8);
+    squee_print_row(row);
 }
