6359208892c380a3bcca4c4e80ff19d2766b9884
Frederick.Ollinger <follinge@gmail.com>
2026-08-11 16:45:24 -0700

    Add Row Takes CLI Args Again

--- sandbox/add-row.sh
+++ sandbox/add-row.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-lldb ./squee-add-row John Doe 42 4.25 file.db
-#./squee-add-row John Doe 42 4.25 file.db
+lldb -- ./squee-add-row John Doe 42 4.25 file.db
+#./squee-add-row Bob Cratcht 65 20.71 file.db
+#./squee-add-row Captain "Meat Head" 65 20.71 file.db
--- sandbox/squee-add-row.c
+++ sandbox/squee-add-row.c
@@ -1,5 +1,6 @@
 #include <fcntl.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -17,28 +18,42 @@ int main(int argc, char* argv[]) {
         printf("\n");
         return(0);
     }
-    char *cols[argc];
+    int num = argc - 2;
+    char **cols = malloc((num + 1) * sizeof(char *));
+
+    printf("add_row() number of cols [%i] \n", num);
 
     Database *db = squee_read_database_from_file(argv[argc - 1]);
-    squee_print_rows(db->table->row);
 
     int c = 0;
-    for (int i = 1; i < argc - 1; i++) {
-        // printf("add_row() number of cols [%i] [%s] \n", i, argv[i]);
-        cols[c] = argv[i];
+    for (int i = 1; i <= num; i++) {
+        printf("add_row() number of cols [%i] [%s] \n", i, argv[i]);
+        cols[c] = strdup(argv[i]);
         c = c + 1;
     }
 
-	char *cols_data1[] = {"John", "Doe", "42", "4.25"};
-    Row *row = squee_create_row(db->table, cols_data1, 4);
+    for (int i = 0; i < 4; i++) {
+        printf("data() number of cols [%i] [%s] \n", i, cols[i]);
+        // free(cols_data1[i]);
+    }
+
+	// char *cols_data1[] = {"John", "Doe", "42", "4.25"};
+    Row *row = squee_create_row(db->table, cols, 3);
     squee_append_row(db->table, row);
 
+    /*
 	char *cols_data2[] = {"Bob", "Cratchet", "35", "1.01"};
     row = squee_create_row(db->table, cols_data2, 4);
     squee_append_row(db->table, row);
 
+	char *cols_data3[] = {"Joe", "Crocker", "69", "1001.20"};
+    row = squee_create_row(db->table, cols_data3, 4);
+    squee_append_row(db->table, row);
+
     squee_print_rows(db->table->row);
+    */
     squee_write_database_to_file(argv[argc - 1], db);
+    // squee_print_rows(db->table->row);
 
     return(0);
 }
