15d089265f4b38b95fe6bd48b122ed954c5b3703
Frederick.Ollinger <follinge@gmail.com>
2026-09-02 17:41:19 -0700

    Doc Ideal CREATE TABLE command

--- sandbox/create-table.sh
+++ sandbox/create-table.sh
@@ -2,3 +2,13 @@
 
 #./squee-create-table Employees First_Name 2 Last_Name 2 Age 0 Hourly_Rate 1 file.db
 lldb -- ./squee-create-table Employees First_Name CHAR Last_Name CHAR Age INT Hourly_Rate FLOAT file.db
+
+# Ideally we should have:
+
+# ./squeectl CREATE TABLE Employees ( \
+# First_Name CHAR,   \
+# Last_Name CHAR,    \
+# Age INT,           \
+# Hourly_Rate FLOAT, \
+# ) \
+# file.db
